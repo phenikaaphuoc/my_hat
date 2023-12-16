69e9c80301aa5e0bc079f491f6f3009c7ec30945
phuoc <21013187@st.phenikaa-uni.edu.vn>
2023-12-16 20:40:29 +0700

    update file setup

--- phuoc_setup.py
+++ phuoc_setup.py
@@ -4,17 +4,19 @@ import os
 import gdown
 
 
-# gdown_id_config = ConfigManager().get_pretrain_weight_gdown_id()
-# os.makedirs(gdown_id_config.save_dir,exist_ok = True)
-# for i,id in enumerate(gdown_id_config.id):
-#     try:
-#         path_save = os.path.join(gdown_id_config.save_dir,gdown_id_config.names[i])
-#         if not os.path.exists(path_save):
-#             gdown.download(id= id, output =path_save, quiet=False)
-#     except Exception as E:
-#         print(f"Can download file id {id}")
+gdown_id_config = ConfigManager().get_pretrain_weight_gdown_id()
+os.makedirs(gdown_id_config.save_dir,exist_ok = True)
+for i,id in enumerate(gdown_id_config.id):
+    try:
+        path_save = os.path.join(gdown_id_config.save_dir,gdown_id_config.names[i])
+        if not os.path.exists(path_save):
+            gdown.download(id= id, output =path_save, quiet=False)
+    except Exception as E:
+        print(f"Can download file id {id}")
 
 
-# os.system(f"python script/data_prepare/generate_metadata.py ")
+os.system(f"python script/data_prepare/generate_metadata.py ")
 
-os.system("python hat/train.py -opt options/train/train_Real_HAT_GAN_SRx4_finetune_from_mse_model.yml")
+# os.system("python hat/train.py -opt options/train/train_Real_HAT_GAN_SRx4_finetune_from_mse_model.yml")
+
+os.system("python hat/train.py -opt options/train/train_HAT-L_SRx4_finetune_from_ImageNet_pretrain.yml")
